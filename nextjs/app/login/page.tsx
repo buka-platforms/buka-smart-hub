dd79a21e3a41830101e8d05aa95216c31537240e
Sony AK <sony@sony-ak.com>
2025-11-23 18:23:26 +0700

    feat: implement login page with logo and artwork, add login form and social sign-in options
    - Created a new login page with a logo and artwork illustration.
    - Added a LoginForm component with options to sign in using Google, GitHub, LinkedIn, X, and Discord.
    - Introduced a BackLink component for navigation.
    - Added UI components for buttons, fields, labels, and separators.
    - Included SVG logo for Buka Smart Hub.
    - Updated package dependencies for Radix UI components.

--- nextjs/app/login/page.tsx
+++ nextjs/app/login/page.tsx
@@ -1,3 +1,52 @@
-export default function Login() {
-  return <div>Login</div>;
+import ImageLogo from "@/assets/images/buka-smart-hub-logo.svg";
+import { LoginForm } from "@/components/login-form";
+
+/* eslint-disable @next/next/no-img-element */
+export default function LoginPage() {
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="#" className="flex items-center gap-2 font-medium">
+            <div className="flex size-6 items-center justify-center rounded-md text-primary-foreground">
+              <img src={ImageLogo.src} alt="Buka Smart Hub Logo" />
+            </div>
+            Buka Smart Hub
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+      <div className="relative hidden bg-muted lg:block">
+        <img
+          src={process.env.NEXT_PUBLIC_LOGIN_PAGE_ARTWORK_IMAGE_URL}
+          alt="Artwork illustration"
+          className="absolute inset-0 h-full w-full object-cover brightness-[0.7] dark:brightness-[0.2] dark:grayscale"
+        />
+        <div className="absolute bottom-0 left-0 z-10 w-max bg-black/40 p-2 text-xs text-white">
+          Photo by{" "}
+          <a
+            href={process.env.NEXT_PUBLIC_LOGIN_PAGE_ARTWORK_ARTIST_PROFILE_URL}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline"
+          >
+            {process.env.NEXT_PUBLIC_LOGIN_PAGE_ARTWORK_ARTIST_NAME}
+          </a>{" "}
+          on{" "}
+          <a
+            href={process.env.NEXT_PUBLIC_LOGIN_PAGE_ARTWORK_URL}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline"
+          >
+            Unsplash
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
