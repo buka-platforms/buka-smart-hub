eb346ff62e26279fd5878a7e4f76c54e8be0022e
Sony AK <sony@sony-ak.com>
2025-12-04 11:25:18 +0700

    refactor: enhance Weather component with type definitions and improve readability

--- nextjs/components/Home/Weather.tsx
+++ nextjs/components/Home/Weather.tsx
@@ -3,15 +3,37 @@
 import Link from "next/link";
 import useSWR from "swr";
 
-const fetcher = async (...args: [RequestInfo, RequestInit?]): Promise<any> => {
+interface WeatherData {
+  temperature: number;
+  condition: string;
+  weather: { icon: string; description: string }[];
+  main: {
+    temp: string;
+  };
+  name: string;
+}
+
+const fetcher = async (
+  ...args: [RequestInfo, RequestInit?]
+): Promise<WeatherData> => {
   const res = await fetch(...args);
   return res.json();
 };
 
+interface RequestHeaders {
+  "x-vercel-ip-latitude": string;
+  "x-vercel-ip-longitude": string;
+}
+
 /* eslint-disable @next/next/no-img-element */
-export default function Weather({ requestHeaders }: { requestHeaders: any }) {
+export default function Weather({
+  requestHeaders,
+}: {
+  requestHeaders: RequestHeaders;
+}) {
+  const appId = process.env.NEXT_PUBLIC_OPENWEATHERMAP_APP_ID;
   const { data, error, isLoading } = useSWR(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${requestHeaders["x-vercel-ip-latitude"]}&lon=${requestHeaders["x-vercel-ip-longitude"]}&units=metric&appid=577fc0797fc84f98dcb1f8e5f878e0fc`,
+    `https://api.openweathermap.org/data/2.5/weather?lat=${requestHeaders["x-vercel-ip-latitude"]}&lon=${requestHeaders["x-vercel-ip-longitude"]}&units=metric&appid=${appId}`,
     fetcher,
     {
       revalidateOnFocus: true,
@@ -22,7 +44,7 @@ export default function Weather({ requestHeaders }: { requestHeaders: any }) {
   return (
     <>
       <Link href="/apps/weather" className="block">
-        <div className="flex items-end text-xs text-[#f5f5f5] opacity-80 hover:opacity-100 md:text-sm">
+        <div className="flex items-end text-xs text-whitesmoke opacity-80 hover:opacity-100 md:text-sm">
           {isLoading ? null : !error && data ? (
             <>
               <div className="flex items-center">
