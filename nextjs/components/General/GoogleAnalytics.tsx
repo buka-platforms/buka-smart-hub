73b4572d53ed6eacbcfc397d5bbbc891ce4e2973
Sony AK <sony@sony-ak.com>
2025-12-07 18:48:59 +0700

    feat: add Google Analytics 4 tracking and its corresponding environment variable.

--- nextjs/components/General/GoogleAnalytics.tsx
+++ nextjs/components/General/GoogleAnalytics.tsx
@@ -4,14 +4,16 @@ export const GoogleAnalytics = () => {
   return (
     <>
       {/* Global site tag (gtag.js) - Google Analytics */}
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-7FS7PZHBTF" />
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_4_MEASUREMENT_ID}`}
+      />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', 'G-7FS7PZHBTF');
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_4_MEASUREMENT_ID}');
         `}
       </Script>
     </>
