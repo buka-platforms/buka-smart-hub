6213971dcbc9b5a8bd91da504aad67b7c69922b6
Sony AK <sony@sony-ak.com>
2025-12-23 03:03:53 +0700

    refactor: remove unused components and integrate radio station directory link

--- nextjs/app/radio/[slug]/RadioPanel.tsx
+++ nextjs/app/radio/[slug]/RadioPanel.tsx
@@ -1,27 +1,14 @@
 "use client";
 
 import { Loading } from "@/components/General/AudioUI";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import { Slider } from "@/components/ui/slider";
 import { mediaAudioStateAtom, radioStationStateAtom } from "@/data/store";
 import type { RadioStation } from "@/data/type";
-import { play, playRandom, stop } from "@/lib/audio";
+import { play, stop } from "@/lib/audio";
 import { useAtomValue, useSetAtom } from "jotai";
-import {
-  Loader2,
-  PlayCircle,
-  Shuffle,
-  StopCircle,
-  Volume1,
-  Volume2,
-  VolumeX,
-} from "lucide-react";
+import { ListMusic, Loader2, PlayCircle, StopCircle } from "lucide-react";
+import Link from "next/link";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 const Play = () => {
   const radioStationState = useAtomValue(radioStationStateAtom);
@@ -59,123 +46,17 @@ const Stop = () => {
   );
 };
 
-const Random = () => {
+const RadioStationDirectory = () => {
   return (
     <>
-      <div
-        onClick={() => playRandom(true)}
-        className="cursor-pointer"
-        title="Random radio station"
-      >
-        <Shuffle
-          className="h-10 w-10 opacity-80 hover:opacity-100 md:h-12 md:w-12"
-          color="#f5f5f5"
-        />
-      </div>
-    </>
-  );
-};
-
-const Volume = () => {
-  const searchParams = useSearchParams();
-  const mediaAudioState = useAtomValue(mediaAudioStateAtom);
-  const setMediaAudioState = useSetAtom(mediaAudioStateAtom);
-  const initialVolume = (() => {
-    const vol = Number(searchParams.get("vol"));
-    if (isNaN(vol))
-      return (mediaAudioState.mediaAudio?.volume as number) * 100 || 0;
-    if (vol > 100) return 100;
-    if (vol < 0) return 0;
-    return vol;
-  })();
-
-  const [volume, setVolume] = useState([initialVolume]);
-
-  const adjustVolume = (value: number[]) => {
-    setVolume(value);
-
-    setMediaAudioState((prev) => {
-      if (prev.mediaAudio) {
-        prev.mediaAudio.volume = value[0] / 100;
-      }
-      return {
-        ...prev,
-        mediaAudio: prev.mediaAudio,
-      };
-    });
-
-    // Save the volume to local storage
-    if (localStorage) {
-      localStorage.setItem("mediaAudioVolume", JSON.stringify(value[0]));
-    }
-  };
-
-  useEffect(() => {
-    if (searchParams.get("vol")) {
-      let definedVolume = Number(searchParams.get("vol"));
-
-      if (definedVolume > 100) {
-        definedVolume = 100;
-      }
-
-      if (definedVolume < 0) {
-        definedVolume = 0;
-      }
-
-      setMediaAudioState((prev) => {
-        if (prev.mediaAudio) {
-          prev.mediaAudio.volume = definedVolume / 100;
-        }
-        return {
-          ...prev,
-          mediaAudio: prev.mediaAudio,
-        };
-      });
-
-      // Save the volume to local storage
-      if (localStorage) {
-        localStorage.setItem(
-          "mediaAudioVolume",
-          definedVolume as unknown as string,
-        );
-      }
-    }
-  }, [searchParams, setMediaAudioState]);
-
-  return (
-    <>
-      <Popover>
-        <PopoverTrigger>
-          <div id="volume" className="cursor-pointer" title="Volume">
-            {Number(mediaAudioState.mediaAudio?.volume) * 100 === 0 ? (
-              <VolumeX
-                className="h-10 w-10 opacity-80 hover:opacity-100 md:h-12 md:w-12"
-                color="#f5f5f5"
-              />
-            ) : Number(mediaAudioState.mediaAudio?.volume) * 100 <= 50 ? (
-              <Volume1
-                className="h-10 w-10 opacity-80 hover:opacity-100 md:h-12 md:w-12"
-                color="#f5f5f5"
-              />
-            ) : Number(mediaAudioState.mediaAudio?.volume) * 100 > 50 ? (
-              <Volume2
-                className="h-10 w-10 opacity-80 hover:opacity-100 md:h-12 md:w-12"
-                color="#f5f5f5"
-              />
-            ) : null}
-          </div>
-        </PopoverTrigger>
-        <PopoverContent className="flex w-44 items-center gap-x-2">
-          <VolumeX size={20} />
-          <Slider
-            value={volume}
-            max={100}
-            step={1}
-            onValueChange={adjustVolume}
+      <Link href="/apps/radio" title="Radio station directory">
+        <div className="cursor-pointer">
+          <ListMusic
+            className="h-10 w-10 opacity-80 hover:opacity-100 md:h-12 md:w-12"
+            color="#f5f5f5"
           />
-          <Volume2 size={20} />
-        </PopoverContent>
-      </Popover>
+        </div>
+      </Link>
     </>
   );
 };
@@ -222,8 +103,7 @@ export default function RadioPanel({
 
       {radioStationState.radioStation && (
         <>
-          {!isInIframe && <Random />}
-          <Volume />
+          {!isInIframe && <RadioStationDirectory />}
           <div className="w-0"></div>
         </>
       )}
