a6292d97680c0cdf82458d8c6a39a1b8811c5b52
Sony AK <sony@sony-ak.com>
2026-01-24 12:25:36 +0700

    refactor: add pointer-events-none class for improved interaction handling in AudioSpectrumCanvas

--- apps/web/components/General/AudioSpectrumCanvas.tsx
+++ apps/web/components/General/AudioSpectrumCanvas.tsx
@@ -3,7 +3,7 @@ export default function AudioSpectrumCanvas() {
     <>
       <div
         id="vis-box"
-        className="fixed right-0 bottom-0 left-0 z-0 m-0 h-60 w-full p-0 md:h-80"
+        className="pointer-events-none fixed right-0 bottom-0 left-0 z-0 m-0 h-60 w-full p-0 md:h-80"
       >
         <canvas id="vis-canvas"></canvas>
       </div>
