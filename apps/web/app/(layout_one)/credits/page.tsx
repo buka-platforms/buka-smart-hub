d38eb271fd921486e1542886e9515d45489ac2c4
Sony AK <sony@sony-ak.com>
2026-03-14 07:46:56 +0700

    refactor(credits): remove Directus from technologies list

--- apps/web/app/(layout_one)/credits/page.tsx
+++ apps/web/app/(layout_one)/credits/page.tsx
@@ -118,7 +118,7 @@ export default async function CreditsPage() {
                   Database & APIs
                 </td>
                 <td className="border px-4 py-2 text-left [[align=center]]:text-center [[align=right]]:text-right">
-                  MySQL, Directus, Redis, Memcached
+                  MySQL, Redis, Memcached
                 </td>
               </tr>
               <tr className="m-0 border-t p-0 even:bg-muted">
